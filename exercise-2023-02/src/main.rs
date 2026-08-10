fa626773c6f82637e58005c9c76decc720f258f0
Your Name <you@example.com>
2026-08-10 18:02:33 +0300

    add game struct

--- exercise-2023-02/src/main.rs
+++ exercise-2023-02/src/main.rs
@@ -1,5 +1,6 @@
 use regex::Regex;
 use std::error::Error;
+use std::fs;
 
 fn extract_number(line: &str, pattern: &str) -> Result<u32, Box<dyn Error>> {
     let re = Regex::new(pattern)?;
@@ -13,28 +14,56 @@ fn extract_number(line: &str, pattern: &str) -> Result<u32, Box<dyn Error>> {
 
 #[derive(Debug)]
 struct Move {
-    blue: u32,
     red: u32,
     green: u32,
+    blue: u32,
 }
 
 impl Move {
-    fn new(record: &str) -> Result<Self, Box<dyn Error>> {
+    fn new(red: u32, green: u32, blue: u32) -> Self {
+        Self { red, green, blue }
+    }
+
+    fn new_from_record(record: &str) -> Result<Self, Box<dyn Error>> {
         let blue = extract_number(record, r"(\d+) blue")?;
         let red = extract_number(record, r"(\d+) red")?;
         let green = extract_number(record, r"(\d+) green")?;
 
-        Ok(Self { blue, green , red})
+        Ok(Self { red, green, blue })
     }
 
-    fn check(self, other: &Move) -> bool {
-        self.blue >= other.blue && self.red >= other.red && self.green >= other.green
+    fn check(&self, maximum: &Move) -> bool {
+        self.blue <= maximum.blue && self.red <= maximum.red && self.green <= maximum.green
+    }
+}
+
+#[derive(Debug)]
+struct Game {
+    number: u32,
+    moves: Vec<Move>,
+}
+
+impl Game {
+    fn new(record: &str) -> Result<Self, Box<dyn Error>> {
+        let number = extract_number(record, r"Game (\d+)")?;
+        let moves = record
+            .split(';')
+            .map(|single_move| Move::new_from_record(single_move))
+            .collect::<Result<Vec<_>, _>>()?;
+
+        Ok(Self { number, moves })
+    }
+
+    fn check(&self, maximum: &Move) -> bool {
+        self.moves.iter().all(|m| m.check(maximum))
     }
 }
 
 fn main() {
-    let first_move = Move::new("2 blue 1 green").unwrap();
-    let second_move = Move::new("0 blue 1 green").unwrap();
+    let maximum_move = Move::new(12, 13, 14);
+    let input = fs::read_to_string("input.txt").unwrap();
 
-    println!("{:?}", first_move.check(&second_move));
+    let games = input.lines().filter_map(|line| Game::new(line).ok());
+    let sum: u32 = games.filter(|game| game.check(&maximum_move)).map(|game| game.number).sum();
+    println!("Sum: {sum}");
 }
