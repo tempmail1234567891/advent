bc48206bf3129503f87d88f69551a565fa223529
Your Name <you@example.com>
2026-08-10 17:34:54 +0300

    implement move struct

--- exercise-2023-02/src/main.rs
+++ exercise-2023-02/src/main.rs
@@ -1,3 +1,40 @@
+use regex::Regex;
+use std::error::Error;
+
+fn extract_number(line: &str, pattern: &str) -> Result<u32, Box<dyn Error>> {
+    let re = Regex::new(pattern)?;
+    if let Some(caps) = re.captures(line) {
+        let number = caps[1].parse()?;
+        Ok(number)
+    } else {
+        Ok(0)
+    }
+}
+
+#[derive(Debug)]
+struct Move {
+    blue: u32,
+    red: u32,
+    green: u32,
+}
+
+impl Move {
+    fn new(record: &str) -> Result<Self, Box<dyn Error>> {
+        let blue = extract_number(record, r"(\d+) blue")?;
+        let red = extract_number(record, r"(\d+) red")?;
+        let green = extract_number(record, r"(\d+) green")?;
+
+        Ok(Self { blue, green , red})
+    }
+
+    fn check(self, other: &Move) -> bool {
+        self.blue >= other.blue && self.red >= other.red && self.green >= other.green
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let first_move = Move::new("2 blue 1 green").unwrap();
+    let second_move = Move::new("0 blue 1 green").unwrap();
+
+    println!("{:?}", first_move.check(&second_move));
 }
