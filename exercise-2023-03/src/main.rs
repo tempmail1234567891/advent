4598bd8e47ec5086e9c8601641bbe6cb0df8ac0c
Your Name <you@example.com>
2026-08-11 14:04:29 +0300

    add rectangle extraction

--- exercise-2023-03/src/main.rs
+++ exercise-2023-03/src/main.rs
@@ -1,19 +1,34 @@
-use regex::{Regex};
-use std::{fs};
+use regex::Regex;
+use std::fs;
 
+fn extract_rectangle(text: &str, start: usize, end: usize, length: usize) -> String {
+    let mut rectangle = String::new();
+    if length <= start {
+        rectangle += &text[(start - length).saturating_sub(1)..(end - length + 1)];
+    }
+    rectangle += &text[start.saturating_sub(1)..start];
+    rectangle += &text[end..text.len().min(end.saturating_add(1))];
+
+    if end + length <= text.len() {
+        rectangle += &text[(start + length).saturating_sub(1)..text.len().min(end + length + 1)];
+    }
+
+    rectangle
+}
 
 fn calculate_sum(text: &str) -> u32 {
     let re = Regex::new(r"\d+").unwrap();
+    let length = text.lines().next().unwrap().len() + 1;
     let matches: Vec<_> = re
         .find_iter(text)
         .map(|caps| {
-            println!(
-                "Match: {}, start: {}, end: {}",
-                caps.as_str(),
-                caps.start(),
-                caps.end()
-            );
-            caps.as_str().parse::<u32>().unwrap()
+            let rectangle = extract_rectangle(text, caps.start(), caps.end(), length);
+            if rectangle.chars().any(|c| !c.is_ascii_digit() && c != '.' && c!='\n') {
+                caps.as_str().parse::<u32>().unwrap()
+            } else {
+                println!("Invalid: {}, {rectangle}", caps.as_str());
+                0
+            }
         })
         .collect();
 
