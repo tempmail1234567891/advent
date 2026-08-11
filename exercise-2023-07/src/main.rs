95bf565fcb1140360d13d1f10af946da6954d5ea
Your Name <you@example.com>
2026-08-11 18:27:24 +0300

    load hands from input txt

--- exercise-2023-07/src/main.rs
+++ exercise-2023-07/src/main.rs
@@ -1,51 +1,13 @@
-use std::{collections::HashMap, str::Chars};
+use std::fs;
+use crate::hand_type::HandType;
 
-fn generate_vector_for_hand(hand: Chars) -> Vec<u32> {
-    let mut hashmap: HashMap<char, u32> = HashMap::new();
-    for character in hand {
-        *hashmap.entry(character).or_default() += 1;
-    }
-    let mut vector:Vec<u32> = hashmap.values().copied().collect();
-
-    vector.sort_by_key(|count| std::cmp::Reverse(*count));
-    vector
-}
-
-#[derive(Debug)]
-enum HandType {
-    FiveKind,
-    FourKind,
-    FullHouse,
-    ThreeKind,
-    TwoPair,
-    OnePair,
-    HighCard,
-}
-
-impl HandType {
-    fn new(hand: &str) -> Self {
-        let vector = generate_vector_for_hand(hand.chars());
-
-        if vector[0] == 5 {
-            HandType::FiveKind
-        } else if vector[0] == 4 {
-            HandType::FourKind
-        } else if vector[0] == 3 && vector[1] == 2 {
-            HandType::FullHouse
-        } else if vector[0] == 3 && vector[1] == 1 {
-            HandType::ThreeKind
-        } else if vector[0] == 2 && vector[1] == 2 {
-            HandType::TwoPair
-        } else if vector[0] == 2 && vector[1] == 1 {
-            HandType::OnePair
-        } else {
-            HandType::HighCard
-        }
-    }
-}
+mod hand_type;
 
 fn main() {
-    let hand = HandType::new("AJAAJ");
-    println!("{:?}", hand);
+    let input = fs::read_to_string("input.txt").unwrap();
+    for line in input.lines(){
+        let hand = HandType::new(line.split(' ').next().unwrap());
+        println!("{:?}", hand);
+    }
 
 }
