b74487fe25dd8721c222dc33bebfd76ab0988c9b
Your Name <you@example.com>
2026-08-11 18:17:24 +0300

    calculate hand type

--- exercise-2023-07/src/main.rs
+++ exercise-2023-07/src/main.rs
@@ -1,3 +1,60 @@
+use std::{collections::HashMap, str::Chars};
+
+fn generate_vector_for_hand(hand: Chars) -> Vec<u32> {
+    let mut hashmap: HashMap<char, u32> = HashMap::new();
+    for character in hand {
+        *hashmap.entry(character).or_default() += 1;
+    }
+    let mut vector:Vec<u32> = hashmap.values().copied().collect();
+
+    vector.sort_by_key(|count| std::cmp::Reverse(*count));
+    vector
+}
+
+#[derive(Debug)]
+struct Hand {
+    level: HandType
+}
+
+#[derive(Debug)]
+enum HandType {
+    FiveKind,
+    FourKind,
+    FullHouse,
+    ThreeKind,
+    TwoPair,
+    OnePair,
+    HighCard,
+}
+
+impl HandType {
+    fn new(vector: Vec<u32>) -> Self {
+        if vector[0] == 5 {
+            HandType::FiveKind
+        } else if vector[0] == 4 {
+            HandType::FourKind
+        } else if vector[0] == 3 && vector[1] == 2 {
+            HandType::FullHouse
+        } else if vector[0] == 3 && vector[1] == 1 {
+            HandType::ThreeKind
+        } else if vector[0] == 2 && vector[1] == 2 {
+            HandType::TwoPair
+        } else if vector[0] == 2 && vector[1] == 1 {
+            HandType::OnePair
+        } else {
+            HandType::HighCard
+        }
+    }
+}
+
+impl Hand {
+    fn new(hand: Chars) -> Self {
+        let vector = generate_vector_for_hand(hand);
+        Hand {level: HandType::new(vector)}
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let hand = Hand::new("AJAAJ".chars());
+    println!("{:?}", hand);
+
 }
