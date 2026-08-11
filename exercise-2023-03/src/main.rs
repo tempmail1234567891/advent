041a3c79c93757ecbc16b6b534d93ef76f6647ad
Your Name <you@example.com>
2026-08-11 12:36:20 +0300

    calculate sum of all numbers

--- exercise-2023-03/src/main.rs
+++ exercise-2023-03/src/main.rs
@@ -1,3 +1,28 @@
+use regex::{Regex};
+use std::{fs};
+
+
+fn calculate_sum(text: &str) -> u32 {
+    let re = Regex::new(r"\d+").unwrap();
+    let matches: Vec<_> = re
+        .find_iter(text)
+        .map(|caps| {
+            println!(
+                "Match: {}, start: {}, end: {}",
+                caps.as_str(),
+                caps.start(),
+                caps.end()
+            );
+            caps.as_str().parse::<u32>().unwrap()
+        })
+        .collect();
+
+    matches.iter().sum()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input.txt").unwrap();
+    let sum: u32 = calculate_sum(&input);
+
+    println!("Match: {}", sum);
 }
