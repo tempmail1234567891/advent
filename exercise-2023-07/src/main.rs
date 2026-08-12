bc5b75991e77473ce0a2b72f5d1d8bba6e764c23
Your Name <you@example.com>
2026-08-12 11:13:37 +0300

    calculate game set

--- exercise-2023-07/src/main.rs
+++ exercise-2023-07/src/main.rs
@@ -5,7 +5,8 @@ mod hand_type;
 
 #[derive(Debug)]
 struct Hand {
-    hand: HandType,
+    cards: String,
+    level: HandType,
     bet: u32,
 }
 
@@ -13,9 +14,13 @@ impl Hand {
     fn new(record: &str) -> Result<Self, hand_type::HandError> {
         let input = record.split_whitespace().take(2).collect::<Vec<_>>();
         if input.len() == 2 {
-            let hand = HandType::new(input[0])?;
+            let level = HandType::new(input[0])?;
             let bet = input[1].parse::<u32>()?;
-            Ok(Self { hand, bet })
+            Ok(Self {
+                cards: String::from(input[0]),
+                level,
+                bet,
+            })
         } else {
             Err(hand_type::HandError::InvalidValue(String::from(
                 "record '{record}' must include 2 values",
@@ -24,10 +29,22 @@ impl Hand {
     }
 }
 
+fn calculate_game_set(input: String) -> u32 {
+    let mut game_set = input
+        .lines()
+        .filter_map(|line| Hand::new(line).ok())
+        .collect::<Vec<_>>();
+
+    game_set.sort_by(|a, b| a.level.cmp(&b.level).then_with(|| b.cards.cmp(&a.cards)));
+
+    let mut sum = 0;
+    for (index, hand) in game_set.iter().enumerate() {
+        sum += hand.bet * (index as u32 + 1)
+    }
+    sum
+}
+
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
-    for line in input.lines() {
-        let hand = Hand::new(line).unwrap();
-        println!("{:?}", hand);
-    }
+    println!("Hand Set: {}",calculate_game_set(input));
 }
