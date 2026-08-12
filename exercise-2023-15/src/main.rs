9005301c52e6b4aa997105cacf66c84464320b69
Your Name <you@example.com>
2026-08-12 13:40:13 +0300

    calculate hash sum

--- exercise-2023-15/src/main.rs
+++ exercise-2023-15/src/main.rs
@@ -1,3 +1,26 @@
+use std::fs;
+
+fn hash(line: &str) -> u32 {
+    let mut number: u32 = 0;
+
+    for character in line.chars() {
+        let code = character as u8;
+        number += code as u32;
+        number *= 17;
+        number %= 256;
+    }
+    number
+}
+
+fn calculate_sum(input: &str) -> u32 {
+    let mut sum = 0;
+    for line in input.split(','){
+        sum += hash(line);
+    }
+    sum
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input.txt").unwrap();
+    println!("Found: {}", calculate_sum(&input));
 }
