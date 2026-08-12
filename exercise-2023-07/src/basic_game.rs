4e5c73a42d07af841bfb00998409077110ccaacd
Your Name <you@example.com>
2026-08-12 11:56:29 +0300

    separate basic game logic

---
+++ exercise-2023-07/src/basic_game.rs
@@ -0,0 +1,30 @@
+use crate::hand::Hand;
+
+fn parse_input(input: String) -> String {
+    input
+        .chars()
+        .map(|c| match c {
+            'A' => 'F',
+            'K' => 'D',
+            'Q' => 'C',
+            'J' => 'B',
+            'T' => 'A',
+            c => c,
+        })
+        .collect()
+}
+
+pub fn calculate_game_set(input: String) -> u32 {
+    let mut game_set = parse_input(input)
+        .lines()
+        .filter_map(|line| Hand::new(line).ok())
+        .collect::<Vec<_>>();
+
+    game_set.sort();
+
+    let mut sum = 0;
+    for (index, hand) in game_set.iter().enumerate() {
+        sum += hand.bet * (index as u32 + 1)
+    }
+    sum
+}
--- exercise-2023-07/src/main.rs
+++ exercise-2023-07/src/main.rs
@@ -1,39 +1,10 @@
-use crate::hand::Hand;
 use std::fs;
 
+mod basic_game;
 mod hand;
 mod hand_type;
 
-fn parse_input(input: String) -> String {
-    input
-        .chars()
-        .map(|c| match c {
-            'A' => 'F',
-            'K' => 'D',
-            'Q' => 'C',
-            'J' => 'B',
-            'T' => 'A',
-            c => c,
-        })
-        .collect()
-}
-
-fn calculate_game_set(input: String) -> u32 {
-    let mut game_set = parse_input(input)
-        .lines()
-        .filter_map(|line| Hand::new(line).ok())
-        .collect::<Vec<_>>();
-
-    game_set.sort();
-
-    let mut sum = 0;
-    for (index, hand) in game_set.iter().enumerate() {
-        sum += hand.bet * (index as u32 + 1)
-    }
-    sum
-}
-
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
-    println!("Hand Set: {}", calculate_game_set(input));
+    println!("Hand Set: {}", basic_game::calculate_game_set(input));
 }
