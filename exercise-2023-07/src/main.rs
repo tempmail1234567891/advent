662ab68ef27ea3d0d10ff3ec4f9776529f3f4a17
Your Name <you@example.com>
2026-08-12 11:50:43 +0300

    parse chars to be in ascii order

--- exercise-2023-07/src/main.rs
+++ exercise-2023-07/src/main.rs
@@ -1,18 +1,30 @@
 use crate::hand::Hand;
 use std::fs;
 
-mod hand_type;
 mod hand;
+mod hand_type;
+
+fn parse_input(input: String) -> String {
+    input
+        .chars()
+        .map(|c| match c {
+            'A' => 'F',
+            'K' => 'D',
+            'Q' => 'C',
+            'J' => 'B',
+            'T' => 'A',
+            c => c,
+        })
+        .collect()
+}
 
 fn calculate_game_set(input: String) -> u32 {
-    let mut game_set = input
+    let mut game_set = parse_input(input)
         .lines()
         .filter_map(|line| Hand::new(line).ok())
         .collect::<Vec<_>>();
 
     game_set.sort();
-
-    println!("{:#?}", game_set);
 
     let mut sum = 0;
     for (index, hand) in game_set.iter().enumerate() {
