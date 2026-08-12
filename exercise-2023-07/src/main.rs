e19fa0515a43c2321cbe9222f17f9c2760160748
Your Name <you@example.com>
2026-08-12 13:19:42 +0300

    rename game file

--- exercise-2023-07/src/main.rs
+++ exercise-2023-07/src/main.rs
@@ -1,10 +1,10 @@
 use std::fs;
 
-mod basic_game;
+mod game;
 mod hand;
 mod hand_type;
 
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
-    println!("Hand Set: {}", basic_game::calculate_game_set(input));
+    println!("Hand Set: {}", game::calculate_game_set(input));
 }
