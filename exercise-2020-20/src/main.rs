8389bb1b86df581fc8cd069696e632b20ea0784a
Your Name <you@example.com>
2026-08-16 18:29:57 +0300

    parse tiles in main

--- exercise-2020-20/src/main.rs
+++ exercise-2020-20/src/main.rs
@@ -1,3 +1,37 @@
+use crate::tile::Tile;
+mod tile;
+
+fn parse_tiles(input: &str) -> Vec<Tile> {
+    input
+        .split("\n\n")
+        .filter_map(|block| {
+            let mut lines = block.lines();
+
+            let header = lines.next().unwrap();
+
+            let number: u32 = header
+                .strip_prefix("Tile ")
+                .unwrap()
+                .strip_suffix(":")
+                .unwrap()
+                .parse()
+                .unwrap();
+
+            let tile = lines.collect::<Vec<_>>().join("\n");
+
+            Tile::new(number, &tile).ok()
+        })
+        .collect()
+}
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("input.txt").unwrap();
+    let tiles = parse_tiles(&input);
+
+    for tile in tiles.iter() {
+        for other in tiles.iter() {
+            if let Some(location) = tile.order(&other){
+                println!("{:?} {:?} {:?}", tile, other, location);
+            }
+        }
+    }
 }
