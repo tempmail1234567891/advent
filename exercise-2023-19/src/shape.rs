80a26098a312924b02f6057292ce39960c78e893
Your Name <you@example.com>
2026-08-16 11:55:20 +0300

    remove unused implemnetation

--- exercise-2023-19/src/shape.rs
+++ exercise-2023-19/src/shape.rs
@@ -1,16 +1,9 @@
 pub struct Shape {
-    pub x:i32,
-    pub m:i32,
-    pub a:i32,
-    pub s:i32,
+    pub x: i32,
+    pub m: i32,
+    pub a: i32,
+    pub s: i32,
 }
-
-impl Shape {
-    pub fn new(x:i32,m:i32,a:i32,s:i32) -> Self {
-        Self { x, m, a, s }
-    }
-}
-
 
 type CheckFn = Box<dyn Fn(&Shape) -> bool + 'static>;
 pub struct ShapeHandler {
