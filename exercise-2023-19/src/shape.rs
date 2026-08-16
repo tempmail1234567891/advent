e608ac3197ac86eb3f5efb23c98cbdf7fe4d9ef2
Your Name <you@example.com>
2026-08-16 11:33:47 +0300

    change typing

--- exercise-2023-19/src/shape.rs
+++ exercise-2023-19/src/shape.rs
@@ -1,12 +1,12 @@
 pub struct Shape {
-    pub x:u32,
-    pub m:u32,
-    pub a:u32,
-    pub s:u32,
+    pub x:i32,
+    pub m:i32,
+    pub a:i32,
+    pub s:i32,
 }
 
 impl Shape {
-    pub fn new(x:u32,m:u32,a:u32,s:u32) -> Self {
+    pub fn new(x:i32,m:i32,a:i32,s:i32) -> Self {
         Self { x, m, a, s }
     }
 }
