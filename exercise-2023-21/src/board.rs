837bf2a4aa85e63dfb4050f2ae1071a2b4ecfc49
Your Name <you@example.com>
2026-08-16 16:18:02 +0300

    extract point out of struct

--- exercise-2023-21/src/board.rs
+++ exercise-2023-21/src/board.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-type BoardType = HashMap<Point, TileType>;
+pub type BoardType = HashMap<Point, TileType>;
 
 #[derive(Debug, PartialEq)]
 pub enum TileType {
@@ -9,8 +9,8 @@ pub enum TileType {
     Marked(usize),
 }
 
-#[derive(Debug, Hash, PartialEq, Eq)]
-struct Point {
+#[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
+pub struct Point {
     x: usize,
     y: usize,
 }
@@ -20,19 +20,19 @@ impl Point {
         Self { x, y }
     }
 
-    fn near(&self, board: &BoardType) -> Vec<Point> {
-        let mut next_points = vec![];
-        
-        let point = Point::new(self.x - 1, self.y);
-        if let Some(tile) = board.get(&point) && *tile == TileType::Grass {
-            next_points.push(point);
-        }
-        next_points
+    fn near(&self) -> Vec<Point> {
+        vec![
+            Point::new(self.x + 1, self.y),
+            Point::new(self.x.saturating_sub(1), self.y),
+            Point::new(self.x, self.y + 1),
+            Point::new(self.x, self.y.saturating_sub(1)),
+        ]
     }
 }
 
 pub struct Board {
     start: Point,
+    length: usize,
     pub board: BoardType,
 }
 
@@ -40,8 +40,10 @@ impl Board {
     pub fn setup(input: &str) -> Self {
         let mut start = Point::new(0, 0);
         let mut board = HashMap::new();
+        let mut length = 0;
 
         for (i, line) in input.lines().enumerate() {
+            length = line.len() -1 ;
             for (j, c) in line.char_indices() {
                 let tile = match c {
                     'S' => {
@@ -56,6 +58,46 @@ impl Board {
             }
         }
 
-        Self { start, board }
+        Self { length, start, board }
     }
+    pub fn walk(&mut self) -> Result<(), String> {
+        walk(&self.start, &mut self.board)
+    }
+
+    pub fn print(&self) {
+        for x in 0..=self.length {
+            for y in 0..=self.length {
+                let point = Point { x, y };
+
+                let character = match self.board.get(&point) {
+                    Some(TileType::Marked(mark)) => mark.to_string(),
+                    Some(TileType::Rock) => "#".to_string(),
+                    Some(TileType::Grass) => ".".to_string(),
+                    None => "?".to_string(),
+                };
+                print!("{}", character);
+            }
+            println!();
+        }
+    }
+}
+
+fn walk(point: &Point, board: &mut BoardType) -> Result<(), String> {
+    println!("{:?}", point);
+    let mark = match board.get(point) {
+        Some(TileType::Marked(mark)) => *mark,
+        Some(_) => return Err("given tile is not marked".to_string()),
+        None => return Err("given point not in board".to_string()),
+    };
+
+    let neighbors = point.near();
+
+    for next in neighbors {
+        if let Some(TileType::Grass) = board.get(&next) {
+            board.insert(next, TileType::Marked(mark + 1));
+            walk(&next, board).ok();
+        }
+    }
+
+    Ok(())
 }
--- exercise-2023-21/src/main.rs
+++ exercise-2023-21/src/main.rs
@@ -1,8 +1,14 @@
-use crate::board::Board;
+use std::collections::HashMap;
+
+use crate::board::{Board, TileType};
 
 mod board;
 
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
-    let board = Board::setup(&input);
+    let mut board = Board::setup(&input);
+    board.print();
+    board.walk();
+
+    board.print();
 }
