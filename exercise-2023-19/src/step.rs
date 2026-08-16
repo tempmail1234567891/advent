78752501c096a5120686cc5f9edca87e14d54eab
Your Name <you@example.com>
2026-08-16 11:55:42 +0300

    remove unused function

--- exercise-2023-19/src/step.rs
+++ exercise-2023-19/src/step.rs
@@ -1,6 +1,5 @@
-use std::fmt::Debug;
 use crate::shape::{Shape, ShapeHandler};
-
+use std::fmt::Debug;
 
 #[derive(Debug)]
 pub struct Step {
@@ -21,20 +20,12 @@ impl Step {
         self.next = Some(Box::new(old));
     }
 
-    pub fn push_right(&mut self, value: ShapeHandler) {
-        let mut node = Step::new(value);
-        node.next = self.next.take();
-        self.next = Some(Box::new(node));
-    }
-
-    pub fn run(&self, shape: &Shape)-> Option<String> {
+    pub fn run(&self, shape: &Shape) -> Option<String> {
         if (self.value.check_fn)(shape) {
             Some(self.value.target.clone())
-        }
-        else if let Some(next) = &self.next {
+        } else if let Some(next) = &self.next {
             next.run(shape)
-        }
-        else {
+        } else {
             None
         }
     }
