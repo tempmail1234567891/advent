9fe21b041516ed045031a85b73d366088ab9e0ea
Your Name <you@example.com>
2026-08-17 15:36:51 +0300

    save lines as string and not number

--- exercise-2020-20/src/tile.rs
+++ exercise-2020-20/src/tile.rs
@@ -1,43 +1,25 @@
 use crate::direction::{Direction, RelativeLocation};
 
-#[derive(Debug, Hash, PartialEq, Eq, Copy, Clone)]
+#[derive(Debug, Clone)]
 pub struct Card {
-    top: u16,
-    bottom: u16,
-    left: u16,
-    right: u16,
+    top: String,
+    bottom: String,
+    left: String,
+    right: String,
 }
 
 impl Card {
-    fn new(top: u16, bottom: u16, left: u16, right: u16) -> Self {
+    fn new(top: &str, bottom: &str, left: &str, right: &str) -> Self {
         Self {
-            top,
-            bottom,
-            left,
-            right,
-        }
-    }
-
-    fn convert(line: &str) -> Result<u16, String> {
-        if line.len() > 16 {
-            Err("line length must be smaller than 16".to_string())
-        } else {
-            let mut number = 0;
-            for (i, character) in line.char_indices() {
-                match character {
-                    '.' => {}
-                    '#' => {
-                        number |= 1 << i;
-                    }
-                    _ => return Err("invalid character found".to_string()),
-                }
-            }
-            Ok(number)
+            top: top.to_string(),
+            bottom: bottom.to_string(),
+            left: left.to_string(),
+            right: right.to_string(),
         }
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub struct Tile {
     pub id: u32,
     direction: Direction,
@@ -47,45 +29,40 @@ pub struct Tile {
 
 impl Tile {
     pub fn new(id: u32, top: &str, bottom: &str, left: &str, right: &str) -> Result<Self, String> {
-        let card = Card::new(
-            Card::convert(top)?,
-            Card::convert(bottom)?,
-            Card::convert(left)?,
-            Card::convert(right)?,
-        );
+        let card = Card::new(top, bottom, left, right);
         Ok(Self {
             id,
             direction: Direction::Default,
             source: card,
-            parsed: card.clone(),
+            parsed: Card::new(top, bottom, left, right),
         })
     }
 
     pub fn set_direction(&mut self, direction: Direction) {
         let parsed = match direction {
             Direction::Default => Card::new(
-                self.source.top,
-                self.source.bottom,
-                self.source.left,
-                self.source.right,
+                &self.source.top,
+                &self.source.bottom,
+                &self.source.left,
+                &self.source.right,
             ),
             Direction::XFlip => Card::new(
-                self.source.bottom,
-                self.source.top,
-                !self.source.left,
-                !self.source.right,
+                &self.source.bottom,
+                &self.source.top,
+                &self.source.left.chars().rev().collect::<String>(),
+                &self.source.right.chars().rev().collect::<String>(),
             ),
             Direction::YFlip => Card::new(
-                !self.source.top,
-                !self.source.bottom,
-                self.source.right,
-                self.source.left,
+                &self.source.top.chars().rev().collect::<String>(),
+                &self.source.bottom.chars().rev().collect::<String>(),
+                &self.source.right,
+                &self.source.left,
             ),
             Direction::XYFlip => Card::new(
-                !self.source.bottom,
-                !self.source.top,
-                !self.source.right,
-                !self.source.left,
+                &self.source.bottom.chars().rev().collect::<String>(),
+                &self.source.top.chars().rev().collect::<String>(),
+                &self.source.right.chars().rev().collect::<String>(),
+                &self.source.left.chars().rev().collect::<String>(),
             ),
         };
 
@@ -93,14 +70,19 @@ impl Tile {
         self.parsed = parsed;
     }
 
-    fn check_match(&self, current: u16, straigt: u16, opposite: u16) -> Option<Direction> {
+    fn check_match(
+        &self,
+        current: &String,
+        straigt: &String,
+        opposite: &String,
+    ) -> Option<Direction> {
         if current == straigt {
             Some(Direction::Default)
         } else if current == opposite {
             Some(Direction::XFlip)
-        } else if current == !straigt {
+        } else if *current == straigt.chars().rev().collect::<String>() {
             Some(Direction::YFlip)
-        } else if current == !opposite {
+        } else if *current == opposite.chars().rev().collect::<String>() {
             Some(Direction::XYFlip)
         } else {
             None
@@ -114,16 +96,16 @@ impl Tile {
     ) -> Option<Direction> {
         match location {
             RelativeLocation::Above => {
-                self.check_match(self.parsed.bottom, other.source.top, other.source.bottom)
+                self.check_match(&self.parsed.bottom, &other.source.top, &other.source.bottom)
             }
             RelativeLocation::Below => {
-                self.check_match(self.parsed.top, other.source.bottom, other.source.top)
+                self.check_match(&self.parsed.top, &other.source.bottom, &other.source.top)
             }
             RelativeLocation::OnLeft => {
-                self.check_match(self.parsed.left, other.source.right, other.source.left)
+                self.check_match(&self.parsed.left, &other.source.right, &other.source.left)
             }
             RelativeLocation::OnRight => {
-                self.check_match(self.parsed.right, other.source.left, other.source.right)
+                self.check_match(&self.parsed.right, &other.source.left, &other.source.right)
             }
         }
     }
